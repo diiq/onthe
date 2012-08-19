b9f860ad25521b481656297cc1a7d286889d9ace
Sam Bleckley <diiq.diiq@gmail.com>
2012-08-19 20:57:59 +1200

    Working statements

--- parser.js
+++ parser.js
@@ -4,7 +4,7 @@ var tests = {};
 var log =  console.log;
 
 // See notes_on_pratt
-// At the moment, all operators have rbp === lbp. Is this correct?
+// At the moment, all operators have rbp === lbp. Is this always correct?
 // Aha! No, it is not; see - as infix or prefix.
 
 Array.prototype.peek = function () { return this[this.length - 1]; };
@@ -12,6 +12,7 @@ Array.prototype.peek = function () { return this[this.length - 1]; };
 var token_stream = function (s) { return lexer.tokenize(s).reverse(); };
 
 var expect = function (t, tokens) {
+    log(t, tokens.peek());
     if (tokens.peek().type !== t) {
         throw {name: "expected", info: [t, tokens.peek()]}
     }
@@ -21,8 +22,7 @@ var expect = function (t, tokens) {
 
 var maybe = function (t, tokens) { 
     if (tokens.peek().type !== t) return false;
-    tokens.pop();
-    return true;
+    return tokens.pop();
 }
 
 var expression = function (prev_precedence, tokens) {
@@ -39,6 +39,7 @@ var expression = function (prev_precedence, tokens) {
 }
 
 var fill = function (t, left, tokens) {
+    log("-->", t);
     if (tokens === undefined){
         tokens = left;
         return operators[t.type].prefix(t, tokens)
@@ -58,14 +59,18 @@ var add_operator = function (type, precedence, prefix, postfix) {
 
 var add_infix = function (type, precedence) {
     add_operator(type, precedence,
-                 function(t, tokens){  throw {name: "no"}; }, 
-                 function(t, left, tokens) { return ["infix", t, left, expression(this.precedence, tokens)];});
+                 function (){  throw {name: "no"}; }, 
+                 function (t, left, tokens) { 
+                     return ["infix", t, left, expression(this.precedence, tokens)];
+                 });
 };
 
 var add_prefix = function (type, precedence) {
     add_operator(type, precedence,
-                 function(t, tokens){return ["prefix", t, expression(this.precedence, tokens)] }, 
-                 function(t, left, tokens) { throw {name: "no"}; });
+                 function(t, tokens){
+                     return ["prefix", t, expression(this.precedence, tokens)];
+                 }, 
+                 function () { throw {name: "no"}; });
 };
 
 var add_infix_class = function () {
@@ -74,10 +79,12 @@ var add_infix_class = function () {
     }
 }
 
+// Operators for expressions:
+
 add_operator("(literal)", 0,
               function(t, tokens){ return ["literal", t]; }, 
               function() { throw {name: "literal_operator"}});
-add_operator("name", 0,
+add_operator("(name)", 0,
               function(t, tokens){ return ["literal", t]; }, 
               function() { throw {name: "literal_operator"}});
 
@@ -166,6 +173,67 @@ add_operator("{", 0, function(t, tokens){
 
 add_operator("indent", -1);
 add_operator("dedent", -1);
+add_operator("dent", -1);
+add_operator(":", -1);
 
+// OK, time for statements!
+
+add_infix_class("=", "+=", "-=", 1); // magically takes care of assignment.
+
+var statements = {};
+statements["break"] = function (tokens) {
+    t = maybe("(name)");
+    return ["break", t];
+}
+
+statements["if"] = function (tokens) {
+    var test, then, els;
+    test = expression(0, tokens);
+    then = block(tokens)
+    if (maybe("else", tokens)) {
+        if (maybe("if", tokens)) {
+            els = statements["if"](tokens);
+        } else {
+            els = block(tokens);
+        }
+    }
+    return ["if", test, then, els];
+}
+
+var block = function (tokens) {
+    var ret;
+    expect(":", tokens);
+    if (maybe("dent", tokens)) {
+        expect("indent", tokens);
+        ret = ["block", statementi(tokens)];
+        expect("dedent", tokens);
+    } else {
+        ret = ["block", [statement(tokens)]];
+    }
+    return ret;
+}
+
+var statement = function (tokens) {
+    var ret;
+    log("this", tokens.peek())
+    if (statements[tokens.peek().type]) {
+       ret = statements[tokens.pop().type](tokens)
+    } else {
+       ret = expression(0, tokens);
+    }
+    expect("dent", tokens);
+    return ret;
+}
+
+var statementi = function (tokens) {
+    var ret = [];
+    while (tokens.peek().type !== "dedent" && tokens.peek().type !== "(end)") {
+       ret.push(statement(tokens));
+    }
+    return ret;
+}
+
+
+exports.statement = statement;
 exports.expression = expression;
 exports.token_stream = token_stream;
