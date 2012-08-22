47c154c3a0fe9d6b3cd3b00300bc1e9c0326be89
Sam Bleckley <diiq.diiq@gmail.com>
2012-08-23 11:48:17 +1200

    Gosh, I hate to do it, but I've pulled tokens out as a global; passing it everywhere was gross, and I couldn't come up with a scenario that needed it. Code smell either way.

--- parser.js
+++ parser.js
@@ -4,23 +4,24 @@ var tests = {};
 var log =  console.log;
 
 // See notes_on_pratt
+
 // At the moment, all operators have rbp === lbp. Is this always correct?
 // Aha! No, it is not; see - as infix or prefix.
 
 Array.prototype.peek = function () { return this[this.length - 1]; };
 
-var token_stream = function (s) { return lexer.tokenize(s).reverse(); };
-
+var tokens;
+var token_stream = function (s) { tokens = lexer.tokenize(s).reverse(); };
 // Parsing expressions
 
-var expression = function (prev_precedence, tokens) {
+var expression = function (prev_precedence) {
     var current, left;
     current = tokens.pop();
-    left = fill(current, tokens);
+    left = fill(current);
     next = tokens.peek(); 
     while (prev_precedence < precedence(next)) {
              current = tokens.pop();
-             left = fill(current, left, tokens);
+             left = fill(current, left);
              next = tokens.peek();
     }
     return left;
@@ -28,15 +29,14 @@ var expression = function (prev_precedence, tokens) {
 
 var operators = {}
 
-var fill = function (t, left, tokens) {
-    if (tokens === undefined){
-        tokens = left;
+var fill = function (t, left) {
+    if (left === undefined){
         if (operators[t.type] === undefined) {
             log(t.type, "UNDEFINED!");
         }
-        return operators[t.type].prefix(t, tokens)
+        return operators[t.type].prefix(t)
     }
-    return operators[t.type].infix(t, left, tokens) 
+    return operators[t.type].infix(t, left) 
 }
 
 var precedence = function (t) {
@@ -52,17 +52,17 @@ var add_operator = function (type, precedence, prefix, infix) {
 var add_infix = function (type, precedence) {
     add_operator(type, precedence,
                  function (t){  log(type, t); throw {name: "no"}; }, 
-                 function (t, left, tokens) { 
-                     return ["infix", ["literal", t], left, expression(this.precedence, tokens)];
+                 function (t, left) { 
+                     return ["infix", ["literal", t], left, expression(this.precedence)];
                  });
 };
 
 var add_prefix = function (type, precedence) {
     add_operator(type, precedence,
-                 function(t, tokens){
-                     return ["prefix", t, expression(this.precedence, tokens)];
+                 function(t){
+                     return ["prefix", t, expression(this.precedence)];
                  }, 
-                 function () { throw {name: "no"}; });
+                 function (t) { log(type, t); throw {name: "no"}; });
 };
 
 var add_infix_class = function () {
@@ -73,23 +73,23 @@ var add_infix_class = function () {
 
 var add_functiony = function(trigger) {
     add_prefix(trigger, 1);
-    operators[trigger].prefix = function (t, tokens) {
+    operators[trigger].prefix = function (t) {
         var args = [], code
-        if (maybe("(", tokens)) {
-            if (!maybe(")", tokens)){
+        if (maybe("(")) {
+            if (!maybe(")")){
                 do {
-                    args.push(expression(0, tokens));
-                } while(maybe(",", tokens));
-                expect(")", tokens);
+                    args.push(expression(0));
+                } while(maybe(","));
+                expect(")");
             }
         }     
-        code = block(tokens);
+        code = block();
         return [trigger, args, code];
     }
 }
 // Parsing statements
 
-var expect = function (t, tokens) {
+var expect = function (t) {
     if (tokens.peek().type !== t) {
         log([t, tokens.peek()])
         throw {name: "expected", info: [t, tokens.peek()]}
@@ -98,28 +98,28 @@ var expect = function (t, tokens) {
     return true;
 }
 
-var maybe = function (t, tokens) { 
+var maybe = function (t) { 
     if (tokens.peek().type !== t) return false;
     return tokens.pop();
 }
 
 var statements = {};
 
-var statement = function (tokens) {
+var statement = function () {
     var ret;
     if (statements[tokens.peek().type]) {
-       ret = statements[tokens.pop().type](tokens)
+       ret = statements[tokens.pop().type]()
     } else {
-       ret = expression(0, tokens);
+       ret = expression(0);
     }
-    expect("dent", tokens);
+    expect("dent");
     return ret;
 }
 
-var statementi = function (tokens) {
+var statementi = function () {
     var ret = [];
     while (tokens.peek().type !== "dedent" && tokens.peek().type !== "(end)") {
-       ret.push(statement(tokens));
+       ret.push(statement());
     }
     return ret;
 }
@@ -127,10 +127,10 @@ var statementi = function (tokens) {
 // Operators for expressions:
 
 add_operator("(literal)", 0,
-              function(t, tokens){ return ["literal", t]; }, 
+              function(t){ return ["literal", t]; }, 
               function() { throw {name: "literal_operator"}});
 add_operator("(name)", 0,
-              function(t, tokens){ return ["literal", t]; }, 
+              function(t){ return ["literal", t]; }, 
               function() { throw {name: "literal_operator"}});
 
 add_operator("(end)", 0,
@@ -145,9 +145,9 @@ add_infix_class("*", "/", "%", 7);
 add_infix("+", 6);
 add_infix("-", 6);
 operators["-"].prefix = 
-                 function(t, tokens){ 
+                 function(t){ 
                      return ["prefix", t, 
-                             expression(0, tokens)];
+                             expression(0)];
                  }
 
 
@@ -157,57 +157,57 @@ add_infix("and", 3);
 add_infix("or", 2);
 
 add_operator("(", 9, 
-                 function(t, tokens){
+                 function(t){
                      // paren'd expression 
-                     var exp = expression(0, tokens);
-                     expect(")", tokens);
+                     var exp = expression(0);
+                     expect(")");
                      return exp;
                  }, 
-                 function(t, left, tokens) {
+                 function(t, left) {
                      // invocation
-                     if (!maybe(")", tokens)){
-                         var args = [expression(0, tokens)];
-                         while (maybe(",", tokens)) {
-                             args.push(expression(0, tokens));
+                     if (!maybe(")")){
+                         var args = [expression(0)];
+                         while (maybe(",")) {
+                             args.push(expression(0));
                          }
-                         expect(")", tokens);
+                         expect(")");
                      }
                      return ["invocation", left, args];
                  });
 
 add_operator("[", 9, 
-                 function(t, tokens){
+                 function(t){
                      // Array literal
-                     var mems = [expression(0, tokens)];
-                     while (maybe(",", tokens)) {
-                         mems.push(expression(0, tokens));
+                     var mems = [expression(0)];
+                     while (maybe(",")) {
+                         mems.push(expression(0));
                      }
-                     expect("]", tokens);
+                     expect("]");
                      return ["array", mems];
                  }, 
-                 function(t, left, tokens) {
+                 function(t, left) {
                      // Refinement
-                     var arg = expression(0, tokens);
-                     expect("]", tokens);
+                     var arg = expression(0);
+                     expect("]");
                      return ["refinement", left, arg];
                  });
 
 add_infix("{", 0);
-operators["{"].postfix = function(t, tokens){
+operators["{"].postfix = function(t){
                      // Array literal
                      var mems = [];
-                     if (maybe(".", tokens)) {
+                     if (maybe(".")) {
                         mems.push(["object_pair", 
                                    ["literal", tokens.pop()], 
-                                   expression(0, tokens)]);
+                                   expression(0)]);
                      }
-                     while (maybe(",", tokens)) {
-                         expect(".", tokens);
+                     while (maybe(",")) {
+                         expect(".");
                          mems.push(["object_pair", 
                                     ["literal", tokens.pop()], 
-                                    expression(0, tokens)]);
+                                    expression(0)]);
                      }
-                     expect("}", tokens);
+                     expect("}");
                      return ["object", mems];
                  };
 
@@ -215,11 +215,11 @@ operators["{"].postfix = function(t, tokens){
 
 
 add_infix("?", 1);
-operators["?"].infix = function (t, left, tokens) {
+operators["?"].infix = function (t, left) {
     var test, then, els;
-    test = expression(0, tokens);
-    expect(":", tokens);
-    els = expression(0, tokens);
+    test = expression(0);
+    expect(":");
+    els = expression(0);
     return ["?", test, then, els];
 }
 
@@ -231,21 +231,21 @@ add_functiony("constructor");
 
 add_infix_class("=", "+=", "-=", 1); // magically takes care of assignment.
 
-statements["break"] = function (tokens) {
-    t = maybe("(name)", tokens);
+statements["break"] = function () {
+    t = maybe("(name)");
     return ["break", t];
 }
 
-statements["if"] = function (tokens) {
+statements["if"] = function () {
     var test, then, els;
-    test = expression(0, tokens);
-    then = block(tokens);
-    expect("dent", tokens);
-    if (maybe("else", tokens)) {
-        if (maybe("if", tokens)) {
-            els = statements["if"](tokens);
+    test = expression(0);
+    then = block();
+    expect("dent");
+    if (maybe("else")) {
+        if (maybe("if")) {
+            els = statements["if"]();
         } else {
-            els = block(tokens);
+            els = block();
         }
     return ["ife", test, then, els];
     } else {
@@ -254,57 +254,57 @@ statements["if"] = function (tokens) {
     return ["if", test, then];
 }
 
-statements["switch"] = function (tokens) {
+statements["switch"] = function () {
     var sw, cases = [], t, b;
-    sw = expression(0, tokens);
-    expect(":", tokens);
-    expect("dent", tokens);
-    expect("indent", tokens);
-    while (maybe("case", tokens)) {
-        t = expression(0, tokens);
-        b = block(tokens);
+    sw = expression(0);
+    expect(":");
+    expect("dent");
+    expect("indent");
+    while (maybe("case")) {
+        t = expression(0);
+        b = block();
         cases.push(["case", t, b]);
-        maybe("dent", tokens);
+        maybe("dent");
     }
-    if (maybe("default", tokens)){
-        cases.push(["default", block(tokens)]);
+    if (maybe("default")){
+        cases.push(["default", block()]);
     }
-    expect("dent", tokens);
-    expect("dedent", tokens);
+    expect("dent");
+    expect("dedent");
     return ["switch", sw, cases];
 }
 
-var block = function (tokens) {
+var block = function () {
     var ret;
-    expect(":", tokens);
-    if (maybe("dent", tokens)) {
-        expect("indent", tokens);
-        ret = ["block", statementi(tokens)];
-        expect("dedent", tokens);
+    expect(":");
+    if (maybe("dent")) {
+        expect("indent");
+        ret = ["block", statementi()];
+        expect("dedent");
     } else {
-        ret = ["block", [statement(tokens)]];
+        ret = ["block", [statement()]];
     }
     return ret;
 }
 
-statements["for"] = function (tokens) {
+statements["for"] = function () {
     var v, n, from, to, by, b;
-    v = expression(0, tokens); // test for name/
+    v = expression(0); // test for name/
     switch (tokens.pop().type) {
         case "in":
-            n = expression(0, tokens);
-            b = block(tokens);
+            n = expression(0);
+            b = block();
             return ["forin", v, n];
         case "from":
-            from = expression(0, tokens);
-            expect("to", tokens);
-            to = expression(0, tokens);
-            if (maybe("by", tokens)) {
-                by = expression(0, tokens);
+            from = expression(0);
+            expect("to");
+            to = expression(0);
+            if (maybe("by")) {
+                by = expression(0);
             } else {
                 by = ["literal", {type:"(literal)", value:1}];
             }
-            b = block(tokens);
+            b = block();
             return ["for", v, from, to, by, b];
         default:
             throw {name: "bad_for"};
